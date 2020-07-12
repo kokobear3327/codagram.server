12588071baf801aa45b007cb606f1969bcb039f8
RichardPitts1 <Richardepitts@gmail.com>
2020-07-12 16:49:32 -0500

    2

--- models/user.js
+++ models/user.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: string
-    }
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
 })
+
+mongoose.model("User", userSchema)
