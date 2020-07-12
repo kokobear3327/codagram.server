0b93909afc20e3b41ed4a525490153d775708722
RichardPitts1 <Richardepitts@gmail.com>
2020-07-12 16:00:28 -0500

    2

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', ()=>{
+    console.log("connected to mongo yeahh")
+})
+
+mongoose.connection.on('error', (err)=>{
+    console.log("err connecting", err)
+})
+
+const customMiddleWare = ()=>{
+    console.log("middleware executed!!");
+}
+
+app.use(customMiddleWare)
+
+app.get('/', (req,res)=> {
+    res.send('hello world');
+})
+
+app.listen(PORT, ()=>{
+    console.log("server is running on", PORT)
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI: "mongodb+srv://richypitts87:rockets94@cluster0.dtpvf.mongodb.net/test?retryWrites=true&w=majority"
+}
