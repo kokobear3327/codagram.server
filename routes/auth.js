afd3b26ac7edb9b4522c2d973542dd99dcea6d60
RichardPitts1 <Richardepitts@gmail.com>
2020-07-12 20:34:41 -0500

    @

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcrypt');
 
 router.get('/', (req,res) => {
     res.send("hello")
@@ -16,17 +17,21 @@ router.post('/signup', (req, res)=>{
         if(savedUser){
             return res.status(422).json({error:"user already exists"})
         }
-        const user = new User({
-            email,
-            password,
-            name
-        })
-        user.save().then(user=>{
-            res.json({message:"saved successfully"})
-        })
-        .catch(err=>{
-            console.log(err);
-        })
+        bcrypt.hash(password, 12)
+            .then(hashedpassword=>{
+                const user = new User({
+                    email,
+                    password:hashedpassword,
+                    name
+                })
+                user.save().then(user=>{
+                    res.json({message:"saved successfully"})
+                })
+                .catch(err=>{
+                    console.log(err);
+                })
+            })
+
     })
     .catch(err=>{
         console.log(err);
