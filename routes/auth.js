fb22e99c81c0bfe9ec25fc482da3712c30fcef0b
RichardPitts1 <Richardepitts@gmail.com>
2020-07-12 20:22:03 -0500

    2

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router()
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
 
 router.get('/', (req,res) => {
     res.send("hello")
 })
 
 router.post('/signup', (req, res)=>{
-    console.log(req.body)
+    const { name, email, password } = req.body
+    if (!email || !password || !name) {
+        return res.json({error:"please add all the fields"})
+    }
+    User.findOne({email:email}).then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"user already exists"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+        user.save().then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 })
+
 
 module.exports = router
